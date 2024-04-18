5008b7eddce3d0ed5a80163022df1f50799d4c4f
Ojhanaveen <nk9411123@gmail.com>
2024-04-18 17:19:13 +0530

    menuicon

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 page3=document.querySelector("#page3")
 msg=document.querySelector("#msg")
 close=document.querySelector("#close")
+menu_icon=document.querySelector("#menu_icon")
+cross_icon=document.querySelector("#cross_icon")
+pagenav=document.querySelector("#pagenav")
 msg.addEventListener("click",function(){
     page3.style.left="0"
     close.style.opacity="1"
@@ -13,4 +16,20 @@ close.addEventListener("click",function(){
     msg.style.opacity="1"
    
 })
+
+menu_icon.addEventListener("click",function(){
+    pagenav.style.left="0"
+    cross_icon.style.display="block"
+    menu_icon.style.display="none"
+   
+})
+cross_icon.addEventListener("click",function(){
+    pagenav.style.left="-55%"
+    cross_icon.style.display="none"
+    menu_icon.style.display="block"
+   
+})
+
+
+
 
